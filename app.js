e39acf6a09529bf00a7b743f0beea189a725608a
babatech <shahabi1209@gmail.com>
2016-11-04 14:54:00 +0100

    areeb commit

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.io = io;
 // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'hjs');
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
