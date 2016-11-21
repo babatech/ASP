ffdb4b78476fce4b3193990a9e9f6d52727a4bea
shoaib <shoaibkiel@gmail.com>
2016-11-21 23:12:28 +0100

    user login half 2

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,16 @@ router.get('/about', function(req, res, next) {
 /*
 @Todo: Shoaib
  */
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
 
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
 /*
  GET login page.
  */
@@ -48,10 +57,14 @@ router.post('/login', passport.authenticate('local-login', {
 /*
  GET login with facebook request.
  */
-router.get('/loginfacebook', function(req, res, next) {
-    header["title"] = 'Login with facebook';
-    res.render('about', { header: header,user: user,title: 'Login with Facebook'  });
-});
+router.get('/loginfacebook', passport.authenticate('facebook', { scope : 'email' }));
+// handle the callback after facebook has authenticated the user
+router.get('/loginfacebook/callback',
+    passport.authenticate('facebook', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
+
 router.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
