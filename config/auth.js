c3ea6637ba0c3c3d4278da089b127fd8616af98a
babatech <shahabi1209@gmail.com>
2016-11-23 00:50:55 +0100

    facebook app detail set

--- config/auth.js
+++ config/auth.js
@@ -5,21 +5,21 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : 'your-secret-clientID-here', // your App ID
-        'clientSecret'  : 'your-client-secret-here', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'clientID'      : '499247396909833', // your App ID
+        'clientSecret'  : '4d3f997d3839df0361f93cc3488f46a0', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/loginfacebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : 'your-secret-clientID-here',
         'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     }
 
 };
