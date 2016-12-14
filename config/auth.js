69b820b11eace5ed313dde18575d13f241263294
babatech <shahabi1209@gmail.com>
2016-12-14 21:45:18 +0100

    facebook fix for azure deployement

--- config/auth.js
+++ config/auth.js
@@ -7,13 +7,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1564108353615682', // your App ID
         'clientSecret'  : '313b51e736d5c9754bd8575885fce1e1', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/loginfacebook/callback'
+        'callbackURL'   : 'http://asp-fh-kiel.azurewebsites.net/loginfacebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+        'callbackURL'       : 'http://asp-fh-kiel.azurewebsites.net/auth/twitter/callback'
     },
 
 
